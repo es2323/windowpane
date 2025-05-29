d81a716396a1f4e24016bcf398b57f18eed4cad4
es2323 <enoshsolomonn@gmail.com>
2025-05-29 15:14:52 +0100

    Feat: Add Header.vue and landing page

--- windowpane/src/App.vue
+++ windowpane/src/App.vue
@@ -1,26 +1,19 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SiteHeader from './components/SiteHeader.vue'; // Make sure the path is correct
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SiteHeader // Register the component
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div id="app">
+    <SiteHeader />
+    <main>
+      <h2>Welcome to my portfolio!</h2>
+    </main>
+  </div>
+</template>
