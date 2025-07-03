bc3a9007edf99f71574d2934e43464a459637bdf
es2323 <enoshsolomonn@gmail.com>
2025-07-03 12:11:43 +0100

    Task: Preloader text fix

--- src/App.vue
+++ src/App.vue
@@ -312,6 +312,11 @@ a:hover {
   backface-visibility: hidden;
 }
 
+.preloader-name {
+  /* Give the name a tiny delay to start after the initial render */
+  animation-delay: 0.1s;
+}
+
 .preloader-title {
   /* This makes 'Portfolio' appear just after 'Enosh Earnest' */
   animation-delay: 0.4s;
